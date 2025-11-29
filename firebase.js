ec3fc1a1c0fa57f46da76ecf8e4f7e0a99048119
Francis Leonard Mahinay <francismahinay.23ziel2001@gmail.com>
2025-11-29 23:06:24 +0800

    Adding toggle mode between online/offline

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; // use default Auth, no persistence
-import { getDatabase } from "firebase/database";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getDatabase, connectDatabaseEmulator } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDs6eEpYkKzOIbit60mitGDY6qbLMclxvs",
@@ -12,7 +12,31 @@ const firebaseConfig = {
   appId: "1:950000610308:web:a39583249e23784128d951"
 };
 
+
+/*
+Mao ni ang toggle mode sa firebase
+true  = para emulator (offline)
+false = pang live Firebase (online)
+*/                      
+const USE_EMULATOR = true; 
+
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app); // no AsyncStorage persistence
+export const auth = getAuth(app);
+
 export const db = getDatabase(app);
+
+// --------------------------------------
+// CONNECT TO EMULATORS (only if enabled)
+// --------------------------------------
+if (USE_EMULATOR) {
+  console.log("Using Firebase Emulator Mode");
+
+  // Auth Emulator
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+
+  // Realtime Database Emulator
+  connectDatabaseEmulator(db, "127.0.0.1", 9000);
+} else {
+  console.log("Using LIVE Firebase");
+}
